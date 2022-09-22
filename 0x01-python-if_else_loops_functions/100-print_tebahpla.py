5260f09e8c698721f17a72d2d33b5023f1a2b721
Powerisinschool <90867477+ooluwatom@users.noreply.github.com>
2022-09-22 22:56:05 +0100

    task 14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in reversed(range(97, 123)):
     if (i % 2 == 0):
-        print('{:c}'.format(i), end='')
+        print(f'{i:c}', end='')
     else:
-        print('{:c}'.format(i - 32), end='')
+        print(f'{(i-32):c}', end='')
