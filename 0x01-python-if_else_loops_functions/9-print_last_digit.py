7c42077f6a5918413100c105add1df536936d9f2
Powerisinschool <90867477+ooluwatom@users.noreply.github.com>
2022-09-22 16:39:35 +0100

    pycodestyle

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -4,6 +4,6 @@ def print_last_digit(number):
         last_digit = number % 10
     else:
         last_digit = (number * -1) % 10
-    
-    print(last_digit, end ='')
+
+    print(last_digit, end='')
     return last_digit
