4a315d05e6133acdbaaf2c8abef8f90c7ae5b8fd
Powerisinschool <90867477+ooluwatom@users.noreply.github.com>
2022-09-27 21:04:36 +0100

    'correction'

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,13 +6,16 @@ if __name__ == '__main__':
     Prints the number of and list of arguments
     '''
 
-    if len(sys.argv) == 0:
-        print('{:d} arguments.'.format(len(sys.argv)))
-    elif len(sys.argv) == 1:
-        print('{:d} argument:'.format(len(sys.argv)))
-        for i in range(1, len(sys.argv) + 1):
-            print('{:d}:{}'.format(i, sys.argv[i]))
-    else:
-        print('{:d} arguments:'.format(len(sys.argv)))
-        for i in range(1, len(sys.argv) + 1):
-            print('{:d}:{}'.format(i, sys.argv[i]))
+    av = sys.argv
+    l_av = len(av) - 1
+
+    if l_av > 1:
+        print(l_av, 'arguments:')
+        for i in range(1, l_av + 1):
+            print('{:d}: {}'.format(i, av[i]))
+    elif l_av == 1:
+        print(l_av, 'argument:')
+        for i in range(1, l_av + 1):
+            print('{:d}: {}'.format(i, av[i]))
+    elif l_av == 0:
+        print(l_av, 'arguments.')
