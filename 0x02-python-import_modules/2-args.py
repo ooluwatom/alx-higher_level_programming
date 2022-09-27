87480e5adf0cdc78cc95418e376c71bfb82e05c9
Powerisinschool <90867477+ooluwatom@users.noreply.github.com>
2022-09-27 20:59:28 +0100

    changes

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -10,5 +10,5 @@ if __name__ == '__main__':
         print('{:d} arguments.'.format(len(sys.argv)))
     else:
         print('{:d} arguments:'.format(len(sys.argv)))
-        for i in range(len(sys.argv)):
+        for i in range(1, len(sys.argv) + 1):
             print('{:d}:{}'.format(i, sys.argv[i]))
