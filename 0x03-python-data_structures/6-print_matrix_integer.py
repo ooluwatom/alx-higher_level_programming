3c9914d52a7c16b4c3a30703ba367bbdd32962fa
Powerisinschool <90867477+ooluwatom@users.noreply.github.com>
2022-09-28 21:54:37 +0100

    new fix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for element in matrix:
+        print('\n', end='')
         for int in element:
             print('{:d}'.format(int), end='')
-            print('\n', end='')
