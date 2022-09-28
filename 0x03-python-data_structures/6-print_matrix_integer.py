d1359dcabf1036d780201b9dc2dcb1b39e516240
Powerisinschool <90867477+ooluwatom@users.noreply.github.com>
2022-09-28 21:55:49 +0100

    commit

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,4 +3,4 @@ def print_matrix_integer(matrix=[[]]):
     for element in matrix:
         print('\n', end='')
         for int in element:
-            print('{:d}'.format(int), end='')
+            print('{:d}'.format(int), end=' ')
