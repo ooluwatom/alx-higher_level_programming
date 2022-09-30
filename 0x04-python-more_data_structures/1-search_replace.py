de01b3bb3400fb9bdeb7f8a4ac7f2bce0d1cceab
Powerisinschool <90867477+ooluwatom@users.noreply.github.com>
2022-09-30 18:50:07 +0100

    task 1

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    i = 0
+    for i in range(len(my_list)):
+        if my_list[i] == search:
+            my_list[i] == replace
+            i += 1
+    return my_list
