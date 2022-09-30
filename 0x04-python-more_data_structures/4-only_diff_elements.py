2627d4000ac47389c4073afdc73d665e09a28053
Powerisinschool <90867477+ooluwatom@users.noreply.github.com>
2022-09-30 21:13:40 +0100

    fix

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    return [i for i in set_1 if i not in set_2]
+    list_1 = [i for i in set_1 if i in set_2]
+    list_2 = [j for j in set_2 if j in set_1]
+    return [list_1, list_2]
