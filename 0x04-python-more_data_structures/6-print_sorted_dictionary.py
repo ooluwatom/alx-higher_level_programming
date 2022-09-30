74d331abc97738f6e7a92177e18bd939f20d9b3b
Powerisinschool <90867477+ooluwatom@users.noreply.github.com>
2022-09-30 22:58:15 +0100

    fix

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    return a_dictionary.sort()
+    sorted_dictionary = sorted(a_dictionary.items())
+
+    for k, v in sorted_dictionary:
+        print('{0}: {1}'.format(k, v))
