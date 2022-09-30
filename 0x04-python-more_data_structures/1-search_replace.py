3c36ceaf330cad66bdd9925e095ea4aced54eebd
Powerisinschool <90867477+ooluwatom@users.noreply.github.com>
2022-09-30 20:20:32 +0100

    commit

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list =[]
-    i = 0
-    for i in range(len(my_list)):
-        if my_list[i] == search:
-            new_list.append(replace)
-            i += 1
-        else:
-            new_list.append(search)
-    return my_list
+    return [replace if i == search else i for i in my_list]
