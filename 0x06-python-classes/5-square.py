80cb1090d3e6453d241df8b9428ea0d84d901004
Powerisinschool <90867477+ooluwatom@users.noreply.github.com>
2022-10-16 21:40:40 +0100

    Fixes

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -50,7 +50,7 @@ class Square:
     def my_print(self):
         '''Prints the square with the character #'''
         if self.__size == 0:
-            pass
+            print()
         else:
             i, j = 1, 1
             for i in range(self.__size):
