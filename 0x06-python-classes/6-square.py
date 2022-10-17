335b6e2270e46372502867918d769dd452e82a9f
Oluwatomi Olagunju <90867477+ooluwatom@users.noreply.github.com>
2022-10-17 15:15:56 +0100

    pycodestyle

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -28,13 +28,13 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        if type(position) is not tuple \
-            or type(position[0]) is not int \
-            or type(position[1]) is not int \
-            or position[0] < 0 \
-            or position[1] < 0 \
-            or len(position) != 2:
-             raise TypeError("position must be a tuple of 2 positive integers")
+        if (type(position) is not tuple 
+                or type(position[0]) is not int 
+                or type(position[1]) is not int 
+                or position[0] < 0 
+                or position[1] < 0 
+                or len(position) != 2):
+            raise TypeError("position must be a tuple of 2 positive integers")
 
         self.__size = size
         self.__position = position
@@ -58,13 +58,13 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if type(value) is not tuple \
-            or type(value[0]) is not int \
-            or type(value[1]) is not int \
-            or value[0] < 0 \
-            or value[1] < 0 \
-            or len(value) != 2:
-             raise TypeError("position must be a tuple of 2 positive integers")
+        if (type(value) is not tuple 
+                or type(value[0]) is not int 
+                or type(value[1]) is not int 
+                or value[0] < 0 
+                or value[1] < 0 
+                or len(value) != 2):
+            raise TypeError("position must be a tuple of 2 positive integers")
 
         self.__position = value
 
