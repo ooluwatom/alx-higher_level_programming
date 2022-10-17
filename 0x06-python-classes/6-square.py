20653cd87dc1372bf05bcb0d40a6d0a11a9d65d8
Oluwatomi Olagunju <90867477+ooluwatom@users.noreply.github.com>
2022-10-17 15:23:53 +0100

    tried fix

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -33,7 +33,7 @@ class Square:
                 or type(position[1]) is not int
                 or position[0] < 0
                 or position[1] < 0
-                or "tuple index out of range"
+                or IndexError
                 or len(position) < 2):
             raise TypeError("position must be a tuple of 2 positive integers")
 
@@ -64,7 +64,7 @@ class Square:
                 or type(value[1]) is not int
                 or value[0] < 0
                 or value[1] < 0
-                or "tuple index out of range"
+                or IndexError
                 or len(value) < 2):
             raise TypeError("position must be a tuple of 2 positive integers")
 
