85b79f29a5d7d319022cbfb122e7d322c65ef8d3
Powerisinschool <90867477+ooluwatom@users.noreply.github.com>
2022-12-25 14:43:13 +0100

    commit

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,9 +4,9 @@ def add_integer(a, b=98):
     This function adds two integers
     Return - a + b
     '''
-    if type(a) != float or type(a) != int:
+    if type(a) != float and type(a) != int:
         raise TypeError('a must be an integer')
-    elif type(b) != float or type(b) != int:
+    elif type(b) != float and type(b) != int:
         raise TypeError('b must be an integer')
     if type(a) == float:
         a = int(a)
