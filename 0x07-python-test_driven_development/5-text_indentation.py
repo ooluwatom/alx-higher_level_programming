a081b8228b3693523cee960b984b0ebff0d9f10e
Powerisinschool <90867477+ooluwatom@users.noreply.github.com>
2022-12-27 21:45:47 +0100

    new commit

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-'''Function that prints a text with 2 new lines after each of these characters: ., ? and :'''
+'''Function that prints a text with 2 new lines after \
+    each of these characters: ., ? and :'''
 
 
 def text_indentation(text):
+    '''Function to add lines'''
     if type(text) is not str:
         raise TypeError('text must be a string')
     for letter in text:
