b0f5105a76e9b0d83e8d55095d862b8891eed50f
Powerisinschool <90867477+ooluwatom@users.noreply.github.com>
2022-12-27 21:39:40 +0100

    commit

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''Function that prints a text with 2 new lines after each of these characters: ., ? and :'''
+
+
+def text_indentation(text):
+    if type(text) is not str:
+        raise TypeError('text must be a string')
+    for letter in text:
+        if letter == '.' or letter == '?' or letter == ':':
+            text.replace(text[letter + 1], '\n\n')
