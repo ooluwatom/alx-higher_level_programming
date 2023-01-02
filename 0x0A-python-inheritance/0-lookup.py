3b8b8789053a0de72740108d6ae255a7caf457a7
Powerisinschool <90867477+ooluwatom@users.noreply.github.com>
2023-01-02 15:02:43 +0100

    commit

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+'''This function returns a list containing all the methods and attributes belonging to an object'''
+def lookup(obj):
+    '''Function returning list of all the methods belonging to an object'''
+    return dir(obj)
