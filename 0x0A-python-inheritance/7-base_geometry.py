c700d38b0a56bb1143bd0bcd3a1b59a63406ab41
Powerisinschool <90867477+ooluwatom@users.noreply.github.com>
2023-01-03 20:32:05 +0100

    commit

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -12,6 +12,6 @@ class BaseGeometry:
         '''Validates value'''
         self.name = str(name)
         if type(value) is not int:
-            raise TypeError('<name> must be an integer')
+            raise TypeError(f'{name} must be an integer')
         if value <= 0:
-            raise ValueError('<name> must be greater than 0')
+            raise ValueError(f'{name} must be greater than 0')
