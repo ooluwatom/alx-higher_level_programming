a5554226b99b5c4848297bfa0f78d235184fc1ed
Powerisinschool <90867477+ooluwatom@users.noreply.github.com>
2023-01-03 20:15:45 +0100

    commit

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,8 +4,10 @@
 
 
 def is_kind_of_class(obj, a_class):
-    # '''Function that checks whether object is an instance of a class\
-    # or instance of a class that inherited from the class'''
+    '''Function that checks whether object is an \
+        instance of a class that inherited from the class'''
+
+
     if isinstance(obj, a_class) and \
        issubclass(a_class, obj.__class__) is False:
         return True
