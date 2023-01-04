d6681fac08a7079082898193a27f742706552ee1
Powerisinschool <90867477+ooluwatom@users.noreply.github.com>
2023-01-04 23:31:18 +0100

    commit

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''This function reads a text file and prints it'''
+
+
+def read_file(filename=""):
+    '''Reads a text file and prints it'''
+    with open(filename, mode='r', encoding="UTF-8") as f:
+        for line in f:
+            print(line)
