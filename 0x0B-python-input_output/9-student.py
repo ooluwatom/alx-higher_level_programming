d8f2d490160de7a35e6f6f2a3bfe83e8dbcaa7f3
Powerisinschool <90867477+ooluwatom@users.noreply.github.com>
2023-01-09 22:37:37 +0100

    commit

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''Student class'''
+
+
+class Student:
+    '''Student class'''
+    
+    def __init__(self, first_name, last_name, age):
+        '''Constructor'''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        '''Retrieves a dictionary representation of a student instance'''
+        return self.__dict__
