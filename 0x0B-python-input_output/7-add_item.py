2557146a316f5859e73d45a595b1aed3473093e8
Powerisinschool <90867477+ooluwatom@users.noreply.github.com>
2023-01-09 21:49:27 +0100

    commit

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-'''os module and sys modules'''
+'''os modules'''
 from os import path
+'''sys module'''
 from sys import argv
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
