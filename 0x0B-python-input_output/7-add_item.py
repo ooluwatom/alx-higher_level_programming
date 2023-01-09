760bae4bbb64e884f24b054d4c6d5cc6f0a651c7
Powerisinschool <90867477+ooluwatom@users.noreply.github.com>
2023-01-09 21:52:42 +0100

    commit

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 """Add all arguments to a Python list and save them to a file."""
+
+
 from os import path
 """Add all arguments to a Python list and save them to a file."""
+
+
 from sys import argv
 
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
