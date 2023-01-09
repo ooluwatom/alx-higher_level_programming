46f1bda13e155645694ba58978a333ec034f0ed0
Powerisinschool <90867477+ooluwatom@users.noreply.github.com>
2023-01-09 22:14:41 +0100

    commit

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''json module'''
+import json
+
+'''This function returns the dictionary description for\
+     Json serialization of an object'''
+
+
+def class_to_json(obj):
+    def convert_to_serializable(obj):
+        if isinstance(obj, (list, tuple)):
+            return [convert_to_serializable(item) for item in obj]
+        elif isinstance(obj, dict):
+            return {key: convert_to_serializable(value) for key, value in obj.items()}
+        elif isinstance(obj, (int, float, str, bool)):
+            return obj
+        else:
+            raise TypeError("Object of type '%s' is not JSON serializable" % type(obj).__name__)
+
+    return convert_to_serializable(obj)
