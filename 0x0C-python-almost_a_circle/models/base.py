6b2157d4788194ff49f2043b1cc7182c9ee8691e
Powerisinschool <90867477+ooluwatom@users.noreply.github.com>
2023-01-10 19:19:56 +0100

    new_commit

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,12 +5,13 @@
 
 
 class Base:
+    '''Base class'''
     __nb_objects = 0
 
     def __init__(self, id=None):
         if id is not None:
             self.id = id
         else:
-            __nb_objects += 1
-            self.id = __nb_objects
+            self.__nb_objects += 1
+            self.id = self.__nb_objects
  
