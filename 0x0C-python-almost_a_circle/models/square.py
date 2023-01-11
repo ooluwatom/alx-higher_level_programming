fa902f9e33f448b66a30d3405518ce4f48b818bf
Powerisinschool <90867477+ooluwatom@users.noreply.github.com>
2023-01-11 22:03:28 +0100

    n_commit

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -11,3 +11,12 @@ class Square(Rectangle):
     def __str__(self):
         return f'[Square] ({self.id}) {self.__x}/{self.__y} - \
             {self.__height}/{self.__width}'
+
+    @property
+    def size(self):
+        return self.size
+
+    @size.setter
+    def size(self, value):
+        self.__height = value
+        self.__width = value
