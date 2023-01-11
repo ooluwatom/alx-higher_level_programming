b3c6937566a8847a3169e4b5d3f32ca0af19379f
Powerisinschool <90867477+ooluwatom@users.noreply.github.com>
2023-01-11 22:18:54 +0100

    commit

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -6,13 +6,11 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     '''Square Class'''
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(id, x, y, size, size)
-        self.__width = size
-        self.__height = size
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
         return f'[Square] ({self.id}) {self.__x}/{self.__y} - \
-            {self.__height}/{self.__width}'
+            {self.__width}'
 
     @property
     def size(self):
