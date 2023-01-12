3c7be36699bd688d04535c93b53bb013a4d873ec
Powerisinschool <90867477+ooluwatom@users.noreply.github.com>
2023-01-12 16:23:40 +0100

    commit

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,6 +5,7 @@
 
 
 import json
+from os import path
 
 
 class Base:
@@ -62,3 +63,20 @@ class Base:
 
         dummy.update(**dictionary)
         return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        '''Returns a list of instances'''
+        filename = f'{cls.__name__}.json'
+        les = []
+        if path.exists(filename) is False:
+            return les
+
+        with open(filename, mode='r', encoding='utf-8') as f:
+            objs = cls.from_json_string(f.read())
+            instances = []
+
+            for elem in objs:
+                instances.append(cls.create(**elem))
+
+            return instances
