6f7a653425b96f58bcb873599ed1c3df6a2f231b
Powerisinschool <90867477+ooluwatom@users.noreply.github.com>
2023-01-12 15:53:57 +0100

    commit

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -25,3 +25,18 @@ class Base:
             return "[]"
         else:
             return json.JSONEncoder().encode(list_dictionaries)
+
+    def save_to_file(cls, list_objs):
+        '''Write Json string representation of list_objs to a file'''
+        filename = f'{cls.__name__}.json'
+        list_dic = []
+        if list_objs is None:
+            pass
+        else:
+            for i in range(len(list_objs)):
+                list_dic.append(list_objs[i].to_dictionary())
+
+        lists = cls.to_json_string(list_dic)
+
+        with open(filename, 'w') as f:
+            f.write(lists)
