ad00d437aa1e17f7ba218004cb3ab2e2fc52fd09
Powerisinschool <90867477+ooluwatom@users.noreply.github.com>
2023-03-09 16:25:11 +0100

    commit

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
 -- Script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server
 -- Without DESCRIBE OR EXPLAIN
-DESC first_table;
+SHOW CREATE TABLE first_table;
