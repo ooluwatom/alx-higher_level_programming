696a3546766da857dd7d2577fa32e87f2d0a3dd0
Powerisinschool <90867477+ooluwatom@users.noreply.github.com>
2023-03-09 15:49:02 +0100

    commit

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
--- Write a script that creates the database hbtn_0c_0 in your MySQL server
+-- Script that creates the database hbtn_0c_0 in your MySQL server
 -- If the database hbtn_0c_0 already exists, your script should not fail
-CREATE DATABASE hbtn_0c_0;
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
