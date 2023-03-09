a0398608d50bcc6513d70900223964a371de122c
Powerisinschool <90867477+ooluwatom@users.noreply.github.com>
2023-03-09 22:48:06 +0100

    commit

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,7 +1,5 @@
 --  Script that displays the number of records with id = 89 in the table first_table
 -- Counts rows
-SELECT id
-FROM first_table 
-WHERE id = 89
-(SELECT COUNT(id)
-FROM id);
+SELECT COUNT id
+FROM first_table
+WHERE `id` = '89'
