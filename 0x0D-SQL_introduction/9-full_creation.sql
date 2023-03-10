48ebab6727c08f350fcc51393e1a0c637a8cc937
Powerisinschool <90867477+ooluwatom@users.noreply.github.com>
2023-03-10 19:46:31 +0100

    commit

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,14 +1,14 @@
---  Script that inserts a new row in the table first_tablecreates a table second_table
--- Insert new rows
+-- Creates a table `second_table` in the
+-- database `hbtn_0c_0` in MySQL Server and
+-- adds multiples rows.
+
 CREATE TABLE IF NOT EXISTS second_table (
-    id INT, 
-    VARCHAR(256),
+    id INT,
+    name VARCHAR(256),
     score INT
 );
-INSERT INTO
-    second_table(id, name, score)
-VALUES 
-    (1, 'John', 10), 
-    (2, 'Alex', 3), 
-    (3, 'Bob', 14), 
-    (4, 'George', 8);
+
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "George", 8);
