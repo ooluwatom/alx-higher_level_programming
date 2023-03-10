eaac93b000ae45b6c831d88c6ded3a16895353b3
Powerisinschool <90867477+ooluwatom@users.noreply.github.com>
2023-03-10 20:02:14 +0100

    commit

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,5 +1,6 @@
 --  Script that displays all the  records in the table second_table
 -- Displays in descending order of score with score and name
+
 SELECT score, name
 FROM second_table
 GROUP BY `score` DESC;
