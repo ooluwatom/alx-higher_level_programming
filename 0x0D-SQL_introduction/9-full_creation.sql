b20e25309e0edae0ec97b0d3d9847e32359688a0
Powerisinschool <90867477+ooluwatom@users.noreply.github.com>
2023-03-10 19:30:23 +0100

    commit

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,8 +1,8 @@
 --  Script that inserts a new row in the table first_tablecreates a table second_table
 -- Insert new rows
-CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT)
-INSERT INTO second_table
-(id, name, score)
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO `second_table`
+(`id`, `name`, `score`)
 VALUES 
     (1, 'John', 10), 
     (2, 'Alex', 3), 
