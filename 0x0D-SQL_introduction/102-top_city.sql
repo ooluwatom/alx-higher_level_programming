29d87a6c71e180f50e4f21778c55a9ab90c3114c
Oluwatomi Olagunju <90867477+ooluwatom@users.noreply.github.com>
2023-03-13 18:55:03 +0100

    bug

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,6 +1,5 @@
 -- Script that displays the average temperature (Fahrenheit) by city ordered by temperature (descending)
 -- by city ordered by temperature (descending) with top 3 tempertures
 
-SELECT city, AVG(value) as avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC(
-    SELECT TOP (3) * FROM temperatures
-);
+SELECT city, AVG(value) as avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC
+SELECT TOP (3) * FROM temperatures;
