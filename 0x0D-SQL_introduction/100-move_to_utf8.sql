e9b7767fdad8d3360810af11836552cc870375a2
Oluwatomi Olagunju <90867477+ooluwatom@users.noreply.github.com>
2023-03-13 17:30:09 +0100

    UTF8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,4 +2,4 @@
 -- Also convert first_table and name column
 
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
