3cd85299bb125e2bc73fcad344fdb64a052d91fd
Oluwatomi Olagunju <90867477+ooluwatom@users.noreply.github.com>
2023-03-13 19:10:06 +0100

    push

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,4 +1,4 @@
 -- Script that displays the max temp of each state
 -- by city ordered by STATE
 
-SELECT city, MAX(value) as avg_temp FROM temperatures GROUP BY state ORDER BY state DESC;
+SELECT state, MAX(value) as max_temp FROM temperatures GROUP BY state ORDER BY state DESC;
