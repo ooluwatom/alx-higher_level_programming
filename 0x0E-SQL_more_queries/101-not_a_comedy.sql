498b26bc7e16ac95a93a3041c747757f85c9311d
Powerisinschool <90867477+ooluwatom@users.noreply.github.com>
2023-08-16 20:04:41 +0100

    commit

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -8,5 +8,5 @@ SELECT tv_shows.title
 	ON tv_shows.id = tv_show_genres.show_id
 	JOIN tv_genres
 	ON tv_genres.id = tv_show_genres.genre_id
-	WHERE tv_genres.name <> "Comedy")
+	WHERE tv_genres.name = "Comedy")
 	ORDER BY tv_shows.title ASC;
